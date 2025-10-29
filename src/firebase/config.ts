f3712fb5d4f1adece2d4911c35840e7660206e88
Ing. Eduardo Benitez <ing.eduardobenitez00@gmail.com>
2025-10-29 12:47:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1827480670-a09b0",
+  "appId": "1:177194041005:web:802f6167cd0c9275d19024",
+  "apiKey": "AIzaSyDSgDKEI3VvXae8hMfePipJp3L7CUfArBw",
+  "authDomain": "studio-1827480670-a09b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "177194041005"
+};
