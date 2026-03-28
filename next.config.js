9305b6ebb6972f1c8da610ccf5b334934d84129c
Ing. Eduardo Benitez <ing.eduardobenitez00@gmail.com>
2026-03-28 16:18:38 +0000

    el sistema es inseguro La página de inicio no tiene encabezado de respue

--- next.config.js
+++ next.config.js
@@ -47,6 +47,19 @@ const nextConfig = {
       '@radix-ui/react-icons'
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
