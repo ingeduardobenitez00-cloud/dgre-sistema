16739a06b2ccfaf0059db42bd045752ca6fd5652
Ing. Eduardo Benitez <ing.eduardobenitez00@gmail.com>
2026-03-28 16:38:55 +0000

    mejor tambien quita He implementado el encabezado de seguridad HTTP Stri

--- next.config.js
+++ next.config.js
@@ -56,14 +56,6 @@ const nextConfig = {
             value: 'nosniff',
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-          {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
           },
